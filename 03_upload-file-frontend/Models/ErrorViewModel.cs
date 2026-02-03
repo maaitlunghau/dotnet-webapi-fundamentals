6185cba43e0624849c2cd44d0f7b75a92ec2adb7
maaitlunghau <chunhau.py@gmail.com>
2026-02-03 13:16:33 +0700

    chore: reset project structure for systematic learning

--- 03_upload-file-frontend/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace _03_upload_file_frontend.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}

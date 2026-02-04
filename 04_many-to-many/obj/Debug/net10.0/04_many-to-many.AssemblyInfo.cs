afe0af9571453b313bd4d95ad621b4b6f84b1dd8
maaitlunghau <chunhau.py@gmail.com>
2026-02-04 12:52:49 +0700

    feat(lesson-04): add Swagger UI with auto-redirect for API testing

--- 04_many-to-many/obj/Debug/net10.0/04_many-to-many.AssemblyInfo.cs
+++ 04_many-to-many/obj/Debug/net10.0/04_many-to-many.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04_many-to-many")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dcdca1c6017a37196f73c4e19cc47da1b5c5bae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff4db015a68228cae6937059ed0ca54d52971cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("04_many-to-many")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04_many-to-many")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

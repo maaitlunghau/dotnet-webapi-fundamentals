d4e60b4cf9e7d5d2765c9e17290216eeea901ba2
maaitlunghau <chunhau.py@gmail.com>
2026-02-04 18:48:26 +0700

    chore(lesson-05): initital project 05 - authentication and install neccessary packages

--- 02_one-to-many/LModels/obj/Debug/net10.0/LModels.AssemblyInfo.cs
+++ 02_one-to-many/LModels/obj/Debug/net10.0/LModels.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5098e036c81dcc55efaaa0e97855eab4db542c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee2a2fe639a3fdeb7e6c3baf00216f980f74537")]
 [assembly: System.Reflection.AssemblyProductAttribute("LModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

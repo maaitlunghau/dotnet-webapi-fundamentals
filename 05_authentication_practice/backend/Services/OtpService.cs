dd29cebefb725f5edb2c33bd6f605d57573ef608
maaitlunghau <chunhau.py@gmail.com>
2026-02-06 01:51:35 +0700

    feat(lesson-05-practice): implement OTPService with create & verify OTP

---
+++ 05_authentication_practice/backend/Services/OtpService.cs
@@ -0,0 +1,30 @@
+using Shared.Domain;
+
+namespace backend.Services;
+
+public class OtpService
+{
+    private readonly IConfiguration _configuration;
+    public OtpService(IConfiguration configuration)
+        => _configuration = configuration;
+
+
+    public OtpRecord CreateOtp(Guid userId)
+    {
+        var otp = Random.Shared.Next(100000, 999999).ToString();
+        var expiryMinutes = int.Parse(_configuration["OTP:ExpiryMinutes"] ?? "5");
+
+        return new OtpRecord
+        {
+            UserId = userId,
+            OtpCode = otp,
+            ExpireAtUtc = DateTime.UtcNow.AddMinutes(expiryMinutes)
+        };
+    }
+
+
+    public bool VerifyOtp(OtpRecord otpRecord, string inputOtp)
+    {
+        return otpRecord.IsActive && otpRecord.OtpCode == inputOtp;
+    }
+}

ec383bc45496044b3e5fbfd097486a90545fbe36
maaitlunghau <chunhau.py@gmail.com>
2026-02-06 11:35:24 +0700

    feat(lesson-05-practice): register httpClient for calling APIs in client

--- 05_authentication/Services/TokenService.cs
+++ 05_authentication/Services/TokenService.cs
@@ -56,6 +56,7 @@ public class TokenService
         return (accessToken, jti);
     }
 
+    // create refresh token
     public RefreshTokenRecord CreateRefreshtoken(Guid AccountId, string accessTokenJti)
     {
         // tạo thời gian sống refreshToken

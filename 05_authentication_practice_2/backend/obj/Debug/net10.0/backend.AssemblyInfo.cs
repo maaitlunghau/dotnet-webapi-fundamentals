e52dad01b2cf35c61c104852d9021552666281b8
maaitlunghau <chunhau.py@gmail.com>
2026-02-27 10:44:24 +0700

    feat(lesson-05-practice-02): add DOMAIN models (user & refreshTokenRecord)

--- 05_authentication_practice_2/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ 05_authentication_practice_2/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944f6fc3673cc957f3013e3e97a5445fdfee866b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c399a9d91c52088e8c515fcc6161fabf7c0c3ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

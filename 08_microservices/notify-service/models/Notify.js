1920116654fd296933f7a66b602f8182a14970a6
maaitlunghau <chunhau.py@gmail.com>
2026-03-09 19:52:15 +0700

    chore: initial project microservices - notify service (nodejs/expressjs)

---
+++ 08_microservices/notify-service/models/Notify.js
@@ -0,0 +1,24 @@
+const { mongo } = require("mongoose");
+
+const Notify = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    isRead: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});

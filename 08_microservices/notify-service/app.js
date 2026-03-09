4aa604e6b5857c2056fbce82765f6ccd6a9b657a
maaitlunghau <chunhau.py@gmail.com>
2026-03-09 20:35:16 +0700

    feat: config app.js

--- 08_microservices/notify-service/app.js
+++ 08_microservices/notify-service/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const connectDB = require('./configs/db');
+const notificationRouter = require('./routes/notificationRoutes');
 const app = express();
 
+connectDB();
+
+app.use(express.json());
+
+// routes
+app.use('/api/notifications', notificationRouter);
 
 const port = 3000;
 app.listen(port, () => {

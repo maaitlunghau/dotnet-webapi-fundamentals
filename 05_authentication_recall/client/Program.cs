16dd5495318e15ffdf0bb3bdb132276905cac1bc
maaitlunghau <chunhau.py@gmail.com>
2026-03-11 20:12:45 +0700

    chore: config map controllers

--- 05_authentication_recall/client/Program.cs
+++ 05_authentication_recall/client/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -22,7 +23,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Account}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 

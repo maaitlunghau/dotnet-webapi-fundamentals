91540fd645356d46112920076e79d6096aaf6099
maaitlunghau <chunhau.py@gmail.com>
2026-03-23 15:48:57 +0700

    chore: setup for exam project

---
+++ 11_exam/server/Data/DataContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    // public DbSet<Product> { get; set; }
+}
--- 11_exam/server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ 11_exam/server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2df19e2224250808a2226da93555a6ebc8267f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c435ee42ee5a80858544b87e7e1726b5019df74a")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
